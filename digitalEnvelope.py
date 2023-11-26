133f1637e7f6a981d15007058a45dc0dbd4a38d0
Ícaro Gabryel <91147230+icarogby@users.noreply.github.com>
2023-11-26 19:57:07 -0300

    troca de modulo
    Co-authored-by: Luis Henrique <luishenriqueom@users.noreply.github.com>

--- digitalEnvelope.py
+++ digitalEnvelope.py
@@ -1,27 +1,16 @@
-# Criar um envelope assinado
-# § Entrada: arquivo em claro + arquivo da chave RSA pública do destinatário + arquivo da
-# chave RSA privada do remetente + algoritmo simétrico (AES / DES / RC4) e tamanho da
-# chave (quando possível)
-# § Processamento: Gerar chave simétrica temporária/aleatória. Cifrar arquivo em claro com
-# a chave gerada. Assinar o arquivo criptografado com a chave privada do remetente. Cifrar a
-# chave temporária com a chave do destinatário.
-# § Saída: dois arquivos (Um com a chave de seção criptografada e outro do arquivo
-# criptografado assinado).
-
-# Abrir um envelope assinado
-# § Entrada: arquivos com a mensagem assinada e a chave criptografada + arquivo da chave
-# RSA privada do destinatário + arquivo da chave RSA pública do emissor + algoritmo
-# simétrico (AES / DES / RC4)
-# § Processamento: Abrir o envelope, decifrando e verificando a assinatura do mesmo.
-# § Saída: arquivo decifrado e validade ou não da assinatura.
-
-import rsa
-from Crypto.Cipher import AES, DES, ARC4
+from Crypto.Cipher import AES, DES, ARC4, PKCS1_OAEP
+from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
 import binascii
+
+def makeKeys():
+    pass
+
 # todo mudar para bit e n bytes no tamanho da chave
-def makeDigitalEnvelope(plainText: bytes, recipientPublicKey: bytes, senderPrivateKey, symmetricAlgorithm: str, symmetricKeySize: int):
+def makeDigitalEnvelope(plainText: bytes, recipientPublicKey, senderPrivateKey, symmetricAlgorithm: str, symmetricKeySize: int):
     """
     plainText -> The plain text to be encrypted.
     recipientPublicKey -> The recipient's public key to encrypt the symmetric key.
@@ -57,19 +46,21 @@ def makeDigitalEnvelope(plainText: bytes, recipientPublicKey: bytes, senderPriva
             raise ValueError("Invalid symmetric algorithm.")
         
     # Sign the cipher text with the sender's private key:
-    signature = rsa.sign(cipherText, senderPrivateKey, 'SHA-256')
+    hash = SHA256.new(cipherText)
+    signature = pkcs1_15.new(senderPrivateKey).sign(hash)
 
     # Encrypt the symmetric key with the recipient's public key:
-    encryptedKey = rsa.encrypt(symmetricKey, recipientPublicKey)
+    rsaCipher = PKCS1_OAEP.new(recipientPublicKey)
+    encryptedKey = rsaCipher.encrypt(symmetricKey)
 
     # save the encrypted session key to a file
-    with open('encryptedKey.dekey', 'wb') as outputFile:
+    with open('encryptedKey.bin', 'wb') as outputFile:
         outputFile.write(encryptedKey)
 
     # save the encrypted signed message to a file
     with open('encryptedSignedMessage.txt', 'w') as outputFile:
         outputFile.write('--- BEGUIN SIGNATURE ---\n\n' + signature.hex() + '\n\n--- END SIGNATURE ---\n\n--- BEGUIN CIPHER TEXT ---\n\n' + cipherText.hex() + '\n\n--- END CIPHER TEXT ---\n')
-
+# todo passar vetor de inicialização
 def openDigitalEnvelope(encryptedKeyFile: str, encryptedSignedMessageFile: str, recipientPrivateKey: bytes, senderPublicKey: bytes, symmetricAlgorithm: str):
     """
     encryptedKeyFile -> The encrypted symmetric key file.
@@ -87,17 +78,19 @@ def openDigitalEnvelope(encryptedKeyFile: str, encryptedSignedMessageFile: str, 
     with open(encryptedSignedMessageFile, 'r') as inputFile:
         encryptedSignedMessage = inputFile.read()
 
-    # Split the encrypted signed message into signature and cipher text:
+    # Split the encrypted signed message into signature and cipher text: # todo adicionar ao protocolo
     signature = binascii.unhexlify(encryptedSignedMessage.split('--- END SIGNATURE ---')[0].split('--- BEGUIN SIGNATURE ---')[1].strip())
     cipherText = binascii.unhexlify(encryptedSignedMessage.split('--- END CIPHER TEXT ---')[0].split('--- BEGUIN CIPHER TEXT ---')[1].strip())
-
+    # todo adicionar raise erro
     # Decrypt the symmetric key with the recipient's private key:
-    symmetricKey = rsa.decrypt(encryptedKey, recipientPrivateKey)
+    rsaDecipher = PKCS1_OAEP.new(recipientPrivateKey)
+    symmetricKey = rsaDecipher.decrypt(encryptedKey)
 
     # Verify the signature with the sender's public key:
-    if rsa.verify(cipherText, signature, senderPublicKey):
-        print("The signature is authentic.")
-    else:
+    try:
+        pkcs1_15.new(senderPublicKey).verify(SHA256.new(cipherText), signature)
+        print("The signature is authentic.") # todo mudar para raise erro
+    except:
         print("The signature is not authentic.")
 
     # Decrypt the cipher text with the symmetric key:
@@ -109,7 +102,7 @@ def openDigitalEnvelope(encryptedKeyFile: str, encryptedSignedMessageFile: str, 
 
         case "AES":
             cipher = AES.new(symmetricKey, AES.MODE_CBC, bytes([0, 1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15]))
-            print(cipherText)
+
             plainText = unpad(cipher.decrypt(cipherText), AES.block_size)
 
         case "RC4": 
--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 from digitalEnvelope import makeDigitalEnvelope, openDigitalEnvelope
-import rsa 
+from Crypto.PublicKey import RSA
 
 msg = 'Hello world!'.encode('utf-8')
 
 # Carregar a chave pública
-with open('chave_publica_remetente.pem', mode='rb') as chave_publica_arquivo:
-    chave_publica_remetente = rsa.PublicKey.load_pkcs1(chave_publica_arquivo.read())
+with open('./keys/sender/public_key_sender.pem', mode='rb') as chave_publica_arquivo:
+    chave_publica_remetente = RSA.import_key(chave_publica_arquivo.read())
 
 # Carregar a chave privada
-with open('chave_privada_destinatario.pem', mode='rb') as chave_privada_arquivo:
-    chave_privada_destinatario = rsa.PrivateKey.load_pkcs1(chave_privada_arquivo.read())
+with open('./keys/recipient/private_key_recipient.pem', mode='rb') as chave_privada_arquivo:
+    chave_privada_destinatario = RSA.import_key(chave_privada_arquivo.read())
 
 # Carregar a chave pública
-with open('chave_publica_destinatario.pem', mode='rb') as chave_publica_arquivo:
-    chave_publica_destinatario = rsa.PublicKey.load_pkcs1(chave_publica_arquivo.read())
+with open('./keys/recipient/public_key_recipient.pem', mode='rb') as chave_publica_arquivo:
+    chave_publica_destinatario = RSA.import_key(chave_publica_arquivo.read())
 
 # Carregar a chave privada
-with open('chave_privada_remetente.pem', mode='rb') as chave_privada_arquivo:
-    chave_privada_remetente = rsa.PrivateKey.load_pkcs1(chave_privada_arquivo.read())
+with open('./keys/sender/private_key_sender.pem', mode='rb') as chave_privada_arquivo:
+    chave_privada_remetente = RSA.import_key(chave_privada_arquivo.read())
 
-# makeDigitalEnvelope(msg, chave_publica_destinatario, chave_privada_remetente, "AES", 16)
+x = 2
 
-with open('encryptedKey.dekey', mode='rb') as encryptedKeyFile:
-    encryptedKey = encryptedKeyFile.read()
+if x == 1:
+    makeDigitalEnvelope(msg, chave_publica_destinatario, chave_privada_remetente, "AES", 16)
 
-with open('encryptedSignedMessage.txt', mode='r') as encryptedSignedMessageFile:
-    encryptedSignedMessage = encryptedSignedMessageFile.read()
-
-x = openDigitalEnvelope('encryptedKey.dekey', 'encryptedSignedMessage.txt', chave_privada_destinatario, chave_publica_remetente, "AES")
-print(x.decode('utf-8'))
+else:
+    x = openDigitalEnvelope('encryptedKey.bin', 'encryptedSignedMessage.txt', chave_privada_destinatario, chave_publica_remetente, "AES")
+    print(x.decode('utf-8'))
